c665f126c564b732078e5d958f8108b5fad93804
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2024-07-20 02:24:26 +0700

    complete work

---
+++ src/app/components/Task.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Task(tasks) {
+  let styles = {};
+  if(tasks.isDone === true){
+    styles = {"text-decoration":"line-through"};
+  }
+  return (
+    <div className="d-flex p-3 gap-2 align-items-center border-bottom">
+          <span style={styles} >{tasks.title}</span>
+          <button className="btn btn-success">Done</button>
+          <button className="btn btn-danger">Delete</button>
+    </div>
+  )
+}
